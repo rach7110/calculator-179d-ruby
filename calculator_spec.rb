3dd2df5a4ef57f366f53802a8c3848e5b79d779d
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-04 15:39:39 -0600

    tidied up specy replacing examples with before :each do

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -4,6 +4,9 @@ require 'rspec'
 require_relative 'calculator.rb'
 
 describe Calculator do
+  before :each do
+    @calc = Calculator.new(100)
+  end
 
   describe '.initialize' do
     context 'If footprint is not included' do
@@ -13,37 +16,34 @@ describe Calculator do
     end
 
     context 'If only a footprint is included' do
-    	it 'it should set the building\'s footprint' do
-    		calc = Calculator.new(100)
-
-    		expect(calc.footprint).to eq(100)
+    	it 'it should create a calculator instance and set the footprint' do
+    		expect(@calc.footprint).to eq(100)
     	end
 
     	it 'should set the light, hvac, and envelope to false' do
-    		calc = Calculator.new(500)
-
     		expect(calc.light).to be_false
-    		expect(calc.hvac).to be_false
+    		expect(@calc.hvac).to be_false
+        expect(@calc.env).to be_false
     	end
     end
 
     context 'If light, hvac, or envelope is included' do
     	it 'should set light to true' do
-    		calc = Calculator.new(100, true)
+        @calc.light = true
 
-    		expect(calc.light).to be_true
+    		expect(@calc.light).to be_true
     	end
 
     	it 'should set hvac to true' do
-    		calc = Calculator.new(100, true, true)
+        @calc.hvac = true
 
-    		expect(calc.hvac).to be_true
+    		expect(@calc.hvac).to be_true
     	end
 
     	it 'should set envelope to true' do
-    		calc = Calculator.new(100, true, true, true)
+        @calc.env = true
 
-    		expect(calc.env).to be_true
+    		expect(@calc.env).to be_true
     	end
     end
   end
@@ -51,9 +51,7 @@ describe Calculator do
   describe '#benefit_light' do
   	context 'When light is not included.' do
   		it 'The benefit from light should equal 0' do
-  			benny = Calculator.new(100)
-
-  			expect(benny.benefit_light).to eq(0)
+  			expect(@calc.benefit_light).to eq(0)
   		end
   	end
 
